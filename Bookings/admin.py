4484c23f24297ce10d84b87335cb0cc7fa36a474
Tibebe Ababi Demissie <w1942902@westminster.ac.uk>
2024-03-20 12:34:12 +0000

    Set up the Booking and Equipment Models

--- Bookings/admin.py
+++ Bookings/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from Bookings.models import Student, Staff
+from Bookings.models import *
 
 # Register your models here.
+admin.site.register(Admin)
 admin.site.register(Staff)
 admin.site.register(Student)
+admin.site.register(Booking)
+admin.site.register(Equipment)
--- Bookings/models.py
+++ Bookings/models.py
@@ -1,37 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import User; 
+from django.contrib.auth.models import User
+from enum import Enum 
 # Create your models here.
 
 
+class Admin(User):
+    pass    
 
 class Student(User):
     """
         Extension of the Django User model to include more information about students
     """
-    course_dict = {
-    "CS101": "BSc in Computer Science",
-    "ENG201": "BA in English Literature",
-    "MATH301": "BSc in Mathematics",
-    "CHEM101": "BSc in Chemistry",
-    "PHYS202": "BSc in Physics",
-    "HIST102": "BA in History",
-    "PSYC301": "BSc in Psychology",
-    "BIO101": "BSc in Biology",
-    "ART205": "BFA in Fine Arts",
-    "ECON202": "BSc in Economics",
-    "PHIL101": "BA in Philosophy",
-    "BUS301": "BBA",
-    "EDUC202": "Bachelor of Education",
-    "MED101": "MBBS",
-    "LAW201": "LLB",
-    "ARCH301": "BArch",
-    "MUSIC202": "BMus",
-    "NURS101": "BSN",
-    "COMM301": "BA in Communication Studies",
-    "SOC101": "BA in Sociology"}
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = "student_user")
+    course_choices = [
+    ("CS101", "BSc in Computer Science"),
+    ("ENG201", "BA in English Literature"),
+    ("MATH301", "BSc in Mathematics"),
+    ("CHEM101", "BSc in Chemistry"),
+    ("PHYS202", "BSc in Physics"),
+    ("HIST102", "BA in History"),
+    ("PSYC301", "BSc in Psychology"),
+    ("BIO101", "BSc in Biology"),
+    ("ART205", "BFA in Fine Arts"),
+    ("ECON202", "BSc in Economics"),
+    ("PHIL101", "BA in Philosophy"),
+    ("BUS301", "BBA"),
+    ("EDUC202", "Bachelor of Education"),
+    ("MED101", "MBBS"),
+    ("LAW201", "LLB"),
+    ("ARCH301", "BArch"),
+    ("MUSIC202", "BMus"),
+    ("NURS101", "BSN"),
+    ("COMM301", "BA in Communication Studies"),
+    ("SOC101", "BA in Sociology")
+]
     profile_pics = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    current_course = models.CharField(max_length = 10, choices=course_dict)   
+    current_course = models.CharField(max_length = 10, choices=course_choices)   
     student_id = models.CharField(max_length=10, unique=True )
     class Meta:
         verbose_name = "Student"
@@ -41,33 +44,71 @@ class Staff(User):
     """
     Extension of the Django User model to include more information about Staff
     """
-    department_dict = {
-        "CS": "Computer Science Department",
-        "ENG": "English Literature Department",
-        "MATH": "Mathematics Department",
-        "CHEM": "Chemistry Department",
-        "PHYS": "Physics Department",
-        "HIST": "History Department",
-        "PSYC": "Psychology Department",
-        "BIO": "Biology Department",
-        "ART": "Fine Arts Department",
-        "ECON": "Economics Department",
-        "PHIL": "Philosophy Department",
-        "BUS": "Business Administration Department",
-        "EDUC": "Education Department",
-        "MED": "Medical Department",
-        "LAW": "Law Department",
-        "ARCH": "Architecture Department",
-        "MUSIC": "Music Department",
-        "NURS": "Nursing Department",
-        "COMM": "Communication Studies Department",
-        "SOC": "Sociology Department"
-    }
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = "staff_user")
+    department_choices = [
+        ("CS", "Computer Science Department"),
+        ("ENG", "English Literature Department"),
+        ("MATH", "Mathematics Department"),
+        ("CHEM", "Chemistry Department"),
+        ("PHYS", "Physics Department"),
+        ("HIST", "History Department"),
+        ("PSYC", "Psychology Department"),
+        ("BIO", "Biology Department"),
+        ("ART", "Fine Arts Department"),
+        ("ECON", "Economics Department"),
+        ("PHIL", "Philosophy Department"),
+        ("BUS", "Business Administration Department"),
+        ("EDUC", "Education Department"),
+        ("MED", "Medical Department"),
+        ("LAW", "Law Department"),
+        ("ARCH", "Architecture Department"),
+        ("MUSIC", "Music Department"),
+        ("NURS", "Nursing Department"),
+        ("COMM", "Communication Studies Department"),
+        ("SOC", "Sociology Department")
+    ]
+    
+
     profile_pics = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    current_course = models.CharField(max_length = 6, choices=department_dict)   
+    department = models.CharField(max_length = 6, choices=department_choices)   
     staff_id = models.CharField(max_length=10, unique=True )    
     
     class Meta:
         verbose_name = "Staff"
         verbose_name_plural = "Staffs"
+        
+        
+        
+class Booking(models.Model):
+    returnDate = models.DateField()
+    approvalStatus = models.BooleanField()
+    returnStatus = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    admin = models.ForeignKey(Admin, on_delete = models.CASCADE, related_name="approved_by")
+    
+    
+class Equipment(models.Model): 
+    
+    type_choices= [
+    ("PC", "PC/Laptop"),
+    ("VRH", "VR Headset"),
+    ("CS", "Camera/Sensor"),
+    ("PP", "PC Peripherals"),
+    ("Furn", "Furniture"),
+    ("Trip", "Tripod"),
+    ("Oth", "Other"),
+    ("VRC", "VR Controller"),
+    ("PT", "Phones/Tablets")
+]
+    status_choices = [
+        ("Pend", "Pending"),
+        ("Avail", "Available"),
+        ("Decom", "Decommissioned"),
+        ("Unavail", "Unavailable")
+    ]
+    name = models.CharField(max_length = 100)
+    type = models.CharField(max_length = 25, choices = type_choices )
+    location = models.CharField(max_length = 50)
+    status = models.CharField(max_length = 15, choices = status_choices)
+    quantity = models.IntegerField()
+    last_audit = models.DateField()
+    
