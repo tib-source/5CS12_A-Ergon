897a68f75ea4a593934640fe39e6209638c2c5f1
Tibebe <tib.demissie@gmail.com>
2024-04-08 19:11:39 +0100

    Fixed an error with querying the User database

--- Bookings/views.py
+++ Bookings/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 from Bookings.models import Student
+from django.contrib.auth.models import User
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'Bookings/dashboard.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Retrieve the student object and add it to the context
-        student = Student.objects.get(pk=self.request.user.pk)  # Assuming the user is logged in
+        student = User.objects.get(pk=self.request.user.pk)  # Assuming the user is logged in
         context['student'] = student
         return context
