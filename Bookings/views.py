6c3712515078e9c4f86f615f2475f355f99721c6
tib-source <65407303+tib-source@users.noreply.github.com>
2024-04-15 23:06:15 +0100

    Created the equipment list table

--- Bookings/views.py
+++ Bookings/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
-from Bookings.models import Student
+from Bookings.models import Student, Equipment
 from django.contrib.auth.models import User
+
+
+
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'Bookings/dashboard.html'
 
@@ -9,5 +12,8 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         # Retrieve the student object and add it to the context
         student = User.objects.get(pk=self.request.user.pk)  # Assuming the user is logged in
+        all_equipment = Equipment.objects.all()
         context['student'] = student
+        context['equipment'] = all_equipment
+        
         return context
