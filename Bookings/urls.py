4fd627f2f010976e17fe49bf8eb3788850099d8f
Harneet <singhharneet303@gmail.com>
2024-04-30 16:16:34 +0100

    added url for report page

--- Bookings/urls.py
+++ Bookings/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
-from Bookings.views import DashboardView, student_register 
+from Bookings.views import DashboardView, student_register, ReportView 
 from django.views.generic.base import TemplateView  # new
 
 frontend_patterns = [ 
     path('', TemplateView.as_view(template_name='Bookings/landing.html')),
-    path('dashboard', DashboardView.as_view())
+    path('dashboard', DashboardView.as_view()),
+    path('report', ReportView.as_view(), name = 'report')
 
           
 ]
