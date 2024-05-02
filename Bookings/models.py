e7d7f4be1e624d091cd8bf87dc2c532f276f8679
arslanmehmetali <157601069+arslanmehmetali@users.noreply.github.com>
2024-05-02 06:08:28 +0100

    Update models.py

--- Bookings/models.py
+++ Bookings/models.py
@@ -119,3 +119,16 @@ class Report(models.Model):
     def __str__(self):
         return f"{self.user.username}'s {self.report_type} Report"
 
+
+
+class Notification(models.Model):
+    title = models.CharField(max_length=255)
+    message = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
