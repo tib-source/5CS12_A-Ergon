2e15e52186cca32efa61a97d6c252aff622aa053
arslanmehmetali <157601069+arslanmehmetali@users.noreply.github.com>
2024-05-02 06:06:40 +0100

    Update models.py

--- Notifications/models.py
+++ Notifications/models.py
@@ -1,12 +1,1 @@
-from django.db import models
-from django.contrib.auth.models import User
 
-class Notification(models.Model):
-    title = models.CharField(max_length=255)
-    message = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    read = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
