ed6274acafb958410f8a557658e0b72b684b59f3
arslanmehmetali <157601069+arslanmehmetali@users.noreply.github.com>
2024-05-02 06:49:58 +0100

    Delete Approvals/urls.py

--- Approvals/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ApprovalListView, UpdateApprovalRequest
-
-urlpatterns = [
-    path('approvals/', ApprovalListView.as_view(), name='approvals'),
-    path('approvals/update/<int:pk>/', UpdateApprovalRequest.as_view(), name='update_approval'),
-]
