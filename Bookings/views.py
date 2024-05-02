e82dcca705970b1c6b3d025d95d6e2f26e6f16a3
arslanmehmetali <157601069+arslanmehmetali@users.noreply.github.com>
2024-05-02 06:47:33 +0100

    Update views.py

--- Bookings/views.py
+++ Bookings/views.py
@@ -520,7 +520,24 @@ def rebook_item(request, booking_id):
     new_booking.save()
 
     return redirect('booking_history')
+class ApprovalListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    model = ApprovalRequest
+    template_name = 'approvals.html'
+    context_object_name = 'approval_requests'
 
-urlpatterns = [
-    path('notifications/', NotificationListView.as_view(), name='notifications'),
+    def test_func(self):
+        return self.request.user.is_superuser
+
+    def get_queryset(self):
+        return ApprovalRequest.objects.filter(approver=self.request.user, status='pending').order_by('-request_date')
+
+class UpdateApprovalRequest(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = ApprovalRequest
+    fields = ['status']
+    template_name = 'update_approval.html'
+    success_url = reverse_lazy('approvals')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.approver == self.request.user
 ]
